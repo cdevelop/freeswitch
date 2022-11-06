d84ef801b3002d6a7ccbb9f66be80042c59f8f83
xu <cdevelop@qq.com>
2022-11-06 15:08:15 +0800

    SWITCH_THREAD_STACKSIZE 改成1M

--- src/include/switch_types.h
+++ src/include/switch_types.h
@@ -582,7 +582,7 @@ typedef struct switch_filenames switch_filenames;
 SWITCH_DECLARE_DATA extern switch_filenames SWITCH_GLOBAL_filenames;
 
 #define SWITCH_MAX_STACKS 32
-#define SWITCH_THREAD_STACKSIZE 240 * 1024
+#define SWITCH_THREAD_STACKSIZE 1024 * 1024
 #define SWITCH_SYSTEM_THREAD_STACKSIZE 8192 * 1024
 #define SWITCH_MAX_INTERVAL 120	/* we only do up to 120ms */
 #define SWITCH_INTERVAL_PAD 10	/* A little extra buffer space to be safe */
