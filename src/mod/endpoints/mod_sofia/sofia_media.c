33882c381bb1bc95bfc98b9f4bca499f3697f33f
xu <cdevelop@qq.com>
2023-01-14 20:27:12 +0800

    添加sdp修改功能

--- src/mod/endpoints/mod_sofia/sofia_media.c
+++ src/mod/endpoints/mod_sofia/sofia_media.c
@@ -68,6 +68,105 @@ switch_status_t sofia_media_activate_rtp(private_object_t *tech_pvt)
 }
 
 
+static char *do_sdp_replace(const char *sdp, const char *val)
+{
+	const char *string ;
+	size_t string_len;
+	const char *search = val;
+	const char *replace;
+	size_t search_len;
+	size_t replace_len;
+	size_t i, n;
+	size_t dest_len = 0;
+	char *dest, *tmp;
+
+	if(zstr(sdp) || zstr(val)){
+		return NULL;
+	}
+	string = sdp;
+	string_len = strlen(string);
+	replace = strchr(val, '|');
+	if(zstr(replace)){
+		return NULL;
+	}
+	search_len = replace - search;
+	replace += 1;
+	if(zstr(replace)){
+		return NULL;
+	}
+	replace_len = strlen(replace);
+	dest = (char *)malloc(sizeof(char));
+	switch_assert(dest);
+
+	for (i = 0; i < string_len; i++) {
+		if (switch_string_match(string + i, string_len - i, search, search_len) == SWITCH_STATUS_SUCCESS) {
+			for (n = 0; n < replace_len; n++) {
+				dest[dest_len] = replace[n];
+				dest_len++;
+				tmp = (char *)realloc(dest, sizeof(char) * (dest_len + 1));
+				switch_assert(tmp);
+				dest = tmp;
+			}
+			i += search_len - 1;
+		} else {
+			dest[dest_len] = string[i];
+			dest_len++;
+			tmp = (char *)realloc(dest, sizeof(char) * (dest_len + 1));
+			switch_assert(tmp);
+			dest = tmp;
+		}
+	}
+
+	dest[dest_len] = 0;
+
+	return dest;
+}
+
+// sdp_replace_ip=192.168.31.57|192.168.31.1 替换SDP中的IP
+static const char *sofia_sdp_replace(switch_channel_t *channel, const char *sdp)
+{
+	switch_event_header_t *hp;
+	switch_event_t *event;
+	const char *replace = sdp;
+	switch_channel_get_variables(channel, &event);
+
+	for (hp = event->headers; hp; hp = hp->next) {
+		char *var = hp->name;
+		char *val = hp->value;
+
+		if (!strncasecmp(var, "sdp_replace", 11)) {
+			if (hp->idx) {
+				int i;
+				for (i = 0; i < hp->idx; i++) {
+					char *tmp = do_sdp_replace(replace, hp->array[i]);
+					if (tmp) {
+						if (replace != sdp) { free((char *)replace); }
+						replace = tmp;
+					}
+				}
+			} else {
+				char *tmp = do_sdp_replace(replace, val);
+				if (tmp) {
+					if (replace != sdp) { free((char *)replace); }
+					replace = tmp;
+				}
+			}
+		}
+	}
+
+	switch_event_destroy(&event);
+
+	if (replace != sdp) {
+		switch_log_printf(SWITCH_CHANNEL_CHANNEL_LOG(channel), SWITCH_LOG_DEBUG,
+						  "Remote SDP:\n%s\nAfter Replace:\n%s\n", sdp, replace);
+		sdp = switch_core_session_strdup(switch_channel_get_session(channel), replace);
+		free((char *)replace);
+	}
+
+	return sdp;
+}
+
+
 
 switch_status_t sofia_media_tech_media(private_object_t *tech_pvt, const char *r_sdp, switch_sdp_type_t type)
 {
@@ -77,6 +176,9 @@ switch_status_t sofia_media_tech_media(private_object_t *tech_pvt, const char *r
 	if (zstr(r_sdp)) {
 		return SWITCH_STATUS_FALSE;
 	}
+
+    //需要替换SDP打开这个代码
+	// r_sdp = sofia_sdp_replace(tech_pvt->channel, r_sdp);
 
 	if (sofia_media_negotiate_sdp(tech_pvt->session, r_sdp, type)) {
 		if (switch_core_media_choose_port(tech_pvt->session, SWITCH_MEDIA_TYPE_AUDIO, 0) != SWITCH_STATUS_SUCCESS) {
